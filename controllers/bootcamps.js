57203925b459036cbb6e0d12f79d0ea32d156ce4
KopylovVitalik <vitalik.kopylov@gmail.com>
2020-08-31 23:40:59 +0300

    router / controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,36 @@
+// @descr     Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, message: "Show all bootcamps" });
+};
+
+// @descr     Get single bootcamp
+// @route     GET /api/v1/bootcamp/:id
+// @access    Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show bootcamp with id${req.params.id}` });
+};
+
+// @descr     Create single bootcamp
+// @route     POST /api/v1/bootcamp
+// @access    Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, message: "Create new bootcamp" })
+};
+
+// @descr     Update single bootcamp
+// @route     PUT /api/v1/bootcamp/:id
+// @access    Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ message: "Update bootcamp" })
+};
+
+// @descr     Delete single bootcamp
+// @route     PDELETE /api/v1/bootcamp/:id
+// @access    Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ message: "Delete request" })
+};
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Routes
+const bootcamps = require("./routes/bootcamps");
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server running..."));
